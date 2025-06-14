5046ab715d691661378b5ea2e3b5a6c92380f664
NjabuloKukame <njabulokukame60@gmail.com>
2025-06-14 23:00:15 +0200

    feat: enhance UI with updated styles and new icons in Header component

--- shecklist/src/components/Header.jsx
+++ shecklist/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { useDarkMode } from "./DarkMode";
 import "./Header.css";
 import { Link } from "react-router";
-import { BoomBox, SunDim, Search, User } from "lucide-react";
+import { BoomBox, SunDim, Search, User, House } from "lucide-react";
 
 function Header() {
   const { darkMode } = useDarkMode();
@@ -11,20 +11,39 @@ function Header() {
       <h1 className="header-logo">SHECKLIST</h1>
 
       <nav className="header-nav">
-        
-        <Link to={"/"} className="header-link">
-            <Search size={20}/>
-        </Link>
-        <Link to={"/"} className="header-link">
-            <BoomBox size={20}/>
-        </Link>
-        <Link to={"/"} className="header-link">
-            <User size={20}/>
-        </Link>
-        <Link to={"/"} className="header-link">
-            <SunDim size={20}/>
-        </Link>
-        
+        <div className="icon-container">
+          <Link to="/search" className="header-link">
+            <House size={20} />
+          </Link>
+          <span className="icon-label">Home</span>
+        </div>
+
+        <div className="icon-container">
+          <Link to="/search" className="header-link">
+            <Search size={20} />
+          </Link>
+          <span className="icon-label">Search</span>
+        </div>
+
+        <div className="icon-container">
+          <Link to="/" className="header-link">
+            <BoomBox size={20} />
+          </Link>
+          <span className="icon-label">Reviews</span>
+        </div>
+
+        <div className="icon-container">
+          <Link to="/login" className="header-link">
+            <User size={20} />
+          </Link>
+          <span className="icon-label">Login/Signup</span>
+        </div>
+
+        <div className="icon-container">
+          <Link to="#" className="header-link">
+            <SunDim size={20} />
+          </Link>
+        </div>
       </nav>
     </header>
   );
